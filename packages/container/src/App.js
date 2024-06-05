a847bc4d98d5c44ac9a2646776bfa7adde5f2319
Truong Duc Khai <duckhai80@gmail.com>
2024-06-06 02:03:15 +0700

    integration of the container and marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { mount as mountMarketing } from "marketing/MarketingIndex";
+
+console.log(mountMarketing);
 
 export default () => {
   return <h1>Hi there!</h1>;
